1acbfa4a3ca2a9d1f65dcd186e6808d3d2595bf5
utlee <lutae2000@gmail.com>
2022-11-06 23:58:44 +0900

    second commit

---
+++ src/test/java/com/example/learnreactivespring/controller/FluxAndMonoControllerTest.java
@@ -0,0 +1,114 @@
+package com.example.learnreactivespring.controller;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.reactive.WebFluxTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.reactive.server.EntityExchangeResult;
+import org.springframework.test.web.reactive.server.WebTestClient;
+import reactor.core.publisher.Flux;
+import reactor.test.StepVerifier;
+
+import java.util.Arrays;
+import java.util.List;
+import static org.junit.Assert.assertEquals;
+
+@RunWith(SpringRunner.class)
+@WebFluxTest
+public class FluxAndMonoControllerTest {
+
+    @Autowired
+    WebTestClient webTestClient;
+
+    @Test
+    public void fluxapproach1(){
+        Flux<Integer> intergerFlux = webTestClient.get().uri("/flux")
+                .accept(MediaType.APPLICATION_JSON_UTF8)
+                .exchange()
+                .expectStatus().isOk()
+                .returnResult(Integer.class)
+                .getResponseBody();
+
+        StepVerifier.create(intergerFlux)
+                .expectSubscription()
+                .expectNext(1)
+                .expectNext(2)
+                .expectNext(3)
+                .expectNext(4)
+                .verifyComplete();
+    }
+
+    @Test
+    public void fluxapproach2(){
+        webTestClient.get().uri("/flux")
+                .accept(MediaType.APPLICATION_JSON)
+                .exchange()
+                .expectStatus().isOk()
+                .expectHeader().contentType(MediaType.APPLICATION_JSON)
+                .expectBodyList(Integer.class)
+                .hasSize(4);
+    }
+
+    @Test
+    public void Flux_approach3(){
+
+        List<Integer> expectedIntegerList = Arrays.asList(1,2,3,4);
+
+        EntityExchangeResult<List<Integer>> entityExchangeResult = webTestClient.get().uri("/flux")
+                .accept(MediaType.APPLICATION_JSON)
+                .exchange()
+                .expectStatus().isOk()
+                .expectHeader().contentType(MediaType.APPLICATION_JSON)
+                .expectBodyList(Integer.class)
+                .returnResult();
+
+        assertEquals(expectedIntegerList, entityExchangeResult.getResponseBody());
+    }
+
+    @Test
+    public void flux_approach4(){
+        List<Integer> expectedIntegerList = Arrays.asList(1,2,3,4);
+
+        webTestClient.get().uri("/flux")
+                .accept(MediaType.APPLICATION_JSON)
+                .exchange()
+                .expectStatus().isOk()
+                .expectHeader().contentType(MediaType.APPLICATION_JSON)
+                .expectBodyList(Integer.class)
+                .consumeWith((response)->{
+                    assertEquals(expectedIntegerList, response.getResponseBody());
+                });
+    }
+
+    @Test
+    public void fluxStream(){
+        Flux<Long> longStreamFlux = webTestClient.get().uri("/fluxStream")
+                .accept(MediaType.APPLICATION_STREAM_JSON)
+                .exchange()
+                .expectStatus().isOk()
+                .returnResult(Long.class)
+                .getResponseBody();
+
+        StepVerifier.create(longStreamFlux)
+                .expectNext(0L)
+                .expectNext(1L)
+                .thenCancel()
+                .verify();
+    }
+
+    @Test
+    public void mono(){
+        Integer expectValue = new Integer(1);
+
+        webTestClient.get().uri("/mono")
+                .accept(MediaType.APPLICATION_JSON)
+                .exchange()
+                .expectStatus().isOk()
+                .expectBody(Integer.class)
+                .consumeWith((response) -> {
+                    assertEquals(expectValue, response.getResponseBody());
+                });
+    }
+}
